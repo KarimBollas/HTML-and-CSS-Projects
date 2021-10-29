a21ce1d444e651352e4aa46fc79e7efda65ee6a2
KarimBollas <67483536+KarimBollas@users.noreply.github.com>
2021-10-29 12:32:45 -0700

    Save progress until step 149
    Notice at Basic_HTML_1.html

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,1 +1,6 @@
-alert("My first JavaScript code");
+function My_First_Function()
+{
+	var String = "Kiss me, I'm mexican";
+	var result = String.fontcolor("green");				
+	document.getElementById("Mexican").innerHTML = result;				
+}
